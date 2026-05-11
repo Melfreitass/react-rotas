c7f18e983466423503bae780519275f40de26edc
Melfreitass <melissa.g.freitas@aluno.senai.br>
2026-05-11 14:32:43 -0300

    feat: termina atividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
-
-// Editaremos aqui
+import './App.module.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Home from './pages/Home/Home.jsx';
+import Contatos from './pages/Contato/Contato.jsx';
+import Sobre from './pages/Sobre/Sobre.jsx';
+import Servicos from './pages/Servicos/Servicos.jsx';
+import NotFound from './pages/NotFound/NotFound.jsx';
+import Extra from './pages/Extra/Extra.jsx';
 
 function App() {
-  return (
-    // Editaremos aqui
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <NavBar></NavBar>
+                <main style={{ padding: '20px', minHeight: '80vh' }}>
+                    <Routes>
+                        <Route path="/" element={<Home></Home>}></Route>
+                        <Route path="/sobre" element={<Sobre></Sobre>}></Route>
+                        <Route path="/servicos" element={<Servicos></Servicos>}></Route>
+                        <Route path="/equipe" element={<Extra></Extra>}></Route>
+                        <Route path="/contatos" element={<Contatos></Contatos>}></Route>
+                        <Route path="/*" element={<NotFound></NotFound>}></Route>
+                    </Routes>
+                </main>
+                <Footer></Footer>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,4 +1,4 @@
-// Imports aqui
+import { Link } from 'react-router-dom';
 import styles from './NotFound.module.css';
 
 function NotFound() {
@@ -6,8 +6,11 @@ function NotFound() {
         <div className={styles.errorWrapper}>
             <h1>404 ❌</h1>
             <p>Opa! Essa página não existe.</p>
-            {/* Editaremos aqui */}
+            <Link to="/" className="styles.backButton">
+                Voltar para Home
+            </Link>
         </div>
     );
 }
+
 export default NotFound;
